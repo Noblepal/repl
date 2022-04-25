b7b78720480ee2f6330c2411b75bfde810412caa
Joseph Noblepal <zeronillzero@gmail.com>
2022-04-25 12:32:19 +0000

    add digits from user input

--- main.py
+++ main.py
@@ -8,6 +8,20 @@
 
 # print("Your name has "+ str_num_char + " characters")
 
-a = 123
-float_num  = float(a)
-print(float_num)
+# a = 123
+# float_num  = float(a)
+# print(float_num)
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(type(two_digit_number))
+
+a = two_digit_number[0]
+b = two_digit_number[1]
+
+print(int(a) + int(b))
