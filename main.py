df3e8ece9e842e386a36a2b270317144996e42ff
Joseph Noblepal <zeronillzero@gmail.com>
2022-04-25 13:22:41 +0000

    algorithm to calculate tip for split bills

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# 🚨 Don't change the code below 👇
-age = input("What is your current age?")
-# 🚨 Don't change the code above 👆
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
 
-# 365 days in a year
-# 52 weeks in a year
-# 12 months in a year
+print("Welcome to the tip calculator")
 
-years_remaining = 90 - int(age)
+total = float(input("What was the total bill? "))
+percent = float(input("What pecent would you like to give? 10, 12 or 15? "))
+num_people = float(input("How many people split the bill? "))
 
-days = round(years_remaining * 365)
-weeks = round(years_remaining * 52)
-months = round(years_remaining * 12)
+increment = percent / 100 * total
+grand_total = total + increment
 
-message = f"You have {days} days, {weeks} weeks and {months} months left till 90yrs."
-print(message)
+individual_amount = round(grand_total / num_people, 2)
+
+print(f"Each person should pay: {individual_amount}")
