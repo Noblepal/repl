8b9c79db4cc2557556d4ffc8c5c357918904ead6
Noblepal <zeronillzero@gmail.com>
2022-04-26 07:44:30 +0300

    added elif statement

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
-# 🚨 Don't change the code above 👆
+print("Welcome to the roller-coaster!")
+height = int(input("What is your height in cm? "))
 
-# Write your code below this line 👇
-
-if number % 2 == 0:
-    print("This is an even number")
+if height >= 120:
+    print("You can ride the roller-coaster!")
+    age = int(input("What is your age?"))
+    if age < 12:
+        print("Please pay $5")
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
-    print("This is an odd number")
-
+    print("You CANNOT ride the roller-coaster!")
