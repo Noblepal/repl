9401330a504bf69ed507369c6f907dec4738ef8f
Noblepal <zeronillzero@gmail.com>
2022-04-26 08:12:27 +0300

    check if leap year alg

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 # 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
 
-bmi = float(weight) / (float(height) ** 2)
-str_bmi = ""
-
-if bmi < 18.5:
-    str_bmi = "underweight"
-elif bmi < 25:
-    str_bmi = "normal weight"
-elif bmi < 30:
-    str_bmi = "overweight"
-elif bmi < 35:
-    str_bmi = "obese"
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is NOT a leap year")
+    else:
+        print(f"{year} is a leap year")
 else:
-    str_bmi = "clinically obese"
-
-bmi = "{:.1f}".format(bmi)
-
-print(f"Your BMI is {bmi}, you are {str_bmi}")
+    print(f"{year} is NOT a leap year")
