996aac845d068739d1b0ac0bf971ad59ec71a4cc
Noblepal <zeronillzero@gmail.com>
2022-05-06 16:33:11 +0300

    nested lists start

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-import random
+# Nested lists
 
-names_string = input("Give me everybody's names, separated by a comma: ")
+fruits = ["Strawberries", "Grapes"]
+vegetables = ["Potatoes", "Kale"]
 
-names = names_string.split(", ")
+dirty_dozens = [fruits, vegetables]
 
-print(f"Candidates: {names}")
-
-num_people = len(names)
-
-random_number = random.randint(0, num_people - 1)
-
-print(f"{names[random_number]} will be paying the bill!")
-
-# Using random.choice()
-person_to_pay = random.choice(names)
-print(f"Random person to pay: {person_to_pay}")
-
+print(dirty_dozens)
