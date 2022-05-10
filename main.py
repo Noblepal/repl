2a04c3990b329fced509f31095ec8773e282422d
Noblepal <zeronillzero@gmail.com>
2022-05-10 10:46:19 +0300

    highest score

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 # 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split(',')
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
-print(student_heights)
+student_scores = input("Input a list of student scores ").split(',')
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
 # 🚨 Don't change the code above 👆
 
 # Write your code below this row 👇
 
-height_sum = 0
-for height in student_heights:
-    height_sum += height
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
 
-students_count = 0
-for height in student_heights:
-    students_count += 1
-
-average = height_sum / students_count
-
-print(f"The average height of the class is: {round(average)}")
+print(f"The highest score in the class is: {highest_score}")
